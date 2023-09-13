a9e1951c0daf7f78bd8ee80b1a9297904ed4a828
Associate <Associate@roifmrwindt>
2023-09-13 09:03:21 +0530

    added the missed files

---
+++ src/main/java/com/cerebros/exceptions/PortfolioNotFoundException.java
@@ -0,0 +1,8 @@
+package com.cerebros.exceptions;
+
+public class PortfolioNotFoundException extends RuntimeException {
+    public PortfolioNotFoundException(String clientId) {
+
+        super("Portfolio not found for client ID: " + clientId);
+    }
+}
