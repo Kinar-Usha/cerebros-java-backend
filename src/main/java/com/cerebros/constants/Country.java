b3413bd83207b3e74e35a87207279840b9b0ac92
Associate <Associate@roifmrwindt>
2023-09-20 09:58:56 +0530

    update preferences table and add checks in country and preferences

--- src/main/java/com/cerebros/constants/Country.java
+++ src/main/java/com/cerebros/constants/Country.java
@@ -1,5 +1,24 @@
 package com.cerebros.constants;
 
 public enum Country {
-	INDIA, USA, IRELAND
+	INDIA("IN"), USA("US"), IRELAND("IE");
+
+	private String code;
+
+	Country(String code) {
+		this.code = code;
+	}
+
+	public static Country of(String code) {
+		for (Country c : values()) {
+			if (c.code.equals(code)) {
+				return c;
+			}
+		}
+		throw new IllegalArgumentException("Unknown country ISO code. Should be US, IN, IE");
+	}
+
+	public String getCode() {
+		return code;
+	}
 }
--- src/main/java/com/cerebros/integration/ClientDaoImpl.java
+++ src/main/java/com/cerebros/integration/ClientDaoImpl.java
@@ -1,5 +1,44 @@
 package com.cerebros.integration;
 
-public class ClientDaoImpl {
+import com.cerebros.models.Client;
+import com.cerebros.models.Preferences;
+
+public class ClientDaoImpl implements ClientDao {
+
+	@Override
+	public boolean verifyEmail(String email) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void register(Client client) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean login(String email, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Client getClient(String clientId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addClientPreferences(Preferences preferences) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateClientPreferences(Preferences preferences) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
