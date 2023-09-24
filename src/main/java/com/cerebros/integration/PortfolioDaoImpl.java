990c647b3093168b2fef6b01ab86b8dac86a75bf
Associate <Associate@roifmrwindt>
2023-09-24 21:43:09 +0530

    done with portfolio Dao

--- src/main/java/com/cerebros/integration/PortfolioDaoImpl.java
+++ src/main/java/com/cerebros/integration/PortfolioDaoImpl.java
@@ -1,19 +1,14 @@
 package com.cerebros.integration;
 
 import com.cerebros.exceptions.ClientNotFoundException;
-import com.cerebros.models.Order;
 import com.cerebros.models.Portfolio;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository("portfolioDaoImpl")
 public class PortfolioDaoImpl implements PortfolioDao{
@@ -25,7 +20,6 @@ public class PortfolioDaoImpl implements PortfolioDao{
 
     @Override
     public List<Portfolio> getPortfolio(String clientId) throws SQLException, ClientNotFoundException {
-        String sql= "select i.instrumentid, i.description,i.categoryid, p.holdings, pr.bidprice from SCOTT.cerebros_portfolio p inner join SCOTT.cerebros_instruments i on p.instrumentid= i.instrumentid inner join SCOTT.cerebros_prices pr on p.instrumentId =pr.instrumentId WHERE p.clientId=? ORDER BY p.holdings DESC";
         List<Portfolio> portfolioList= mapper.getPortfolio(clientId);
         if(portfolioList.isEmpty()){
             throw new ClientNotFoundException("Client Invalid");
@@ -35,21 +29,39 @@ public class PortfolioDaoImpl implements PortfolioDao{
     }
 
     @Override
-    public void addToPortfolio(Portfolio portfolio, String clientId) {
+    public int addToPortfolio(Portfolio portfolio, String clientId) {
 
-            String orderSql = "insert into cerebros_portfolio(portfolioid, clientid, instrumentid, holdings) values(?,?,?,?)";
+        int rowsInserted=0;
+        try {
+            Map<String, Object> paramMap = new HashMap<>();
+            paramMap.put("clientId", clientId);
+            paramMap.put("portfolio", portfolio);
+          rowsInserted=  mapper.addToPortfolio(paramMap);
+        } catch (Exception e) {
+           throw new DatabaseException("insert failed",e);
+        }
+        return rowsInserted;
 
 
     }
 
     @Override
-    public void updateportfolio(Portfolio portfolio, String clientId) {
+    public int updateportfolio(Portfolio portfolio, String clientId) {
 
-            String orderSql = "UPDATE Cerebros_Portfolio\n" +
-                    "SET holdings = ?\n" +
-                    "WHERE clientId = ?\n" +
-                    "  AND instrumentId = ?";
+        int rowsUpdated=0;
+        try {
+            Map<String, Object> paramMap = new HashMap<>();
+            paramMap.put("clientId", clientId);
+            paramMap.put("portfolio", portfolio);
+            // Call the MyBatis mapper method to insert the portfolio
+           rowsUpdated= mapper.updatePortfolio(paramMap);
 
+        } catch (Exception e) {
+            // Handle the database exception, log it, or rethrow as needed
+            throw new DatabaseException("update failed",e);
+            // Example: throw new DatabaseException("Failed to add portfolio", e);
+        }
+        return rowsUpdated;
 
     }
 }
