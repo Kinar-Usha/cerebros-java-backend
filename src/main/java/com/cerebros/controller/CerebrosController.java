67538581ed0ef6472af70b46e47c23cbe020ac49
Associate <Associate@roifmrwindt>
2023-10-09 14:04:39 +0530

    add cors

--- src/main/java/com/cerebros/controller/CerebrosController.java
+++ src/main/java/com/cerebros/controller/CerebrosController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,7 @@ import com.cerebros.services.TradeService;
 import oracle.jdbc.proxy.annotation.Post;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("")
 public class CerebrosController {
     @Autowired
