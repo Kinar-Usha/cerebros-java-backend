3a59782c25b1cc6a6a2e73299fb617da4922ed2e
Associate <Associate@roifmrwindt>
2023-10-09 14:04:21 +0530

    added crossOrigin

--- src/main/java/com/cerebros/controller/CerebrosController.java
+++ src/main/java/com/cerebros/controller/CerebrosController.java
@@ -3,6 +3,7 @@ package com.cerebros.controller;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 import com.cerebros.exceptions.OrderInvalidException;
 import org.slf4j.Logger;
@@ -10,13 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cerebros.exceptions.ClientAlreadyExistsException;
 import com.cerebros.exceptions.ClientNotFoundException;
@@ -38,6 +33,9 @@ import com.cerebros.services.TradeService;
 
 import oracle.jdbc.proxy.annotation.Post;
 
+
+
+@CrossOrigin( origins = "http://localhost:4200" )
 @RestController
 @RequestMapping("")
 public class CerebrosController {
@@ -188,6 +186,7 @@ public class CerebrosController {
         }
     }
 
+    @CrossOrigin("")
     @GetMapping(value = "/tradehistory/{clientId}")
     public ResponseEntity<List<Trade>> queryTradeHistory(@PathVariable String clientId) {
         try {
@@ -223,6 +222,8 @@ public class CerebrosController {
                     order.getInstrumentId() == null) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
             }
+            String orderId= UUID.randomUUID().toString();
+            order.setOrderId(orderId);
             String clientId = order.getClientId();
             Client client = clientService.getClient(clientId);
             ClientRequest clientRequest = new ClientRequest(client.getPerson().getEmail(), "", "");
@@ -255,7 +256,6 @@ public class CerebrosController {
         }
         return response;
     }
-
     @GetMapping(value = "/portfolio/{clientId}")
     public ResponseEntity<List<Portfolio>> getPortfolio(@PathVariable String clientId) {
         try {
